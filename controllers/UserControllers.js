aa894cfd8717d73a090364e277336eb761d59e64
Bruno Calvi <calvireis@gmail.com>
2022-10-04 21:45:36 -0300

    creating form to update dice

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -3,7 +3,15 @@ class UserController {
     constructor(formId, tableId){
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
+        
         this.onSubmit();
+        this.onEditCancel();
+    }
+
+    onEditCancel() {
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            this.showPanelCreate();
+        })
     }
 
     onSubmit() {
@@ -108,15 +116,31 @@ class UserController {
             <td>${(dataUser.admin == true)? 'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
+
+        tr.querySelector('.btn-edit').addEventListener("click", e=>{
+            console.log(JSON.parse(tr.dataset.user));
+
+            this.showPanelUpdate();
+        });
 
         this.tableEl.appendChild(tr);
 
         this.updateCount();
     
+    }
+
+    showPanelCreate() {
+        document.querySelector('#box-user-create').style.display = 'block';
+        document.querySelector('#box-user-update').style.display = 'none';
+    }
+
+    showPanelUpdate() {
+        document.querySelector('#box-user-create').style.display = 'none';
+        document.querySelector('#box-user-update').style.display = 'block';
     }
 
     updateCount() {
